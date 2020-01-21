9f2c4770643f1f2a70b998555ad94bf50a116e78
Gohar Papinyan <gohar.papinyan@betconstruct.com>
2020-01-21 19:40:45 +0400

    created new branch for refactoring.

--- src/test/java/AssignMentOne/LoginTest.java
+++ src/test/java/AssignMentOne/LoginTest.java
@@ -31,36 +31,38 @@ public class LoginTest {
     private WebDriver driver;
 
     @BeforeMethod
-    public void OpenTheInternet() {
-        setWebDriverPath();
+    public void setUp() {
 
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
+    }
+    @Test
+    public void openTheInternet(){
+
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        WebElement header = driver.findElement(By.className("heading"));
-        String headerText = header.getText();
+        String headerText = driver.findElement(By.className("heading")).getText();
         Assert.assertEquals(headerText,"Welcome to the-internet", "Link is not opened, something went wrong.");
     }
-
     @Test
-    public void OpenFormAuthenticationByLink() {
+    public void openFormAuthenticationByLink() {
+
         driver.get("http://the-internet.herokuapp.com/login");
-       checkOpenFormAuthentication();
+        Assert.assertEquals(getFormAuthenticationHeadText(),"Login Page",
+                "Something went wrong when opening form authentication by link.");
+
     }
 
     @Test
-    public void OpenFormAuthentication() {
-        WebElement formAuthenticationRef = driver.findElement(By.cssSelector("#content > ul > li:nth-child(21) > a"));
-        formAuthenticationRef.click();
-        checkOpenFormAuthentication();
+    public void openFormAuthenticationByClick() {
+       driver.findElement(By.cssSelector("#content > ul > li:nth-child(21) > a")).click();
+        Assert.assertEquals(getFormAuthenticationHeadText(),"Login Page",
+                "Something went wrong when opening form authentication by click.");
     }
     @Test
-    public void Login() {
-        OpenFormAuthentication();
-        WebElement usernameInputField = driver.findElement(By.id("username"));
-        usernameInputField.sendKeys("tomsmith");
-        WebElement passwordInputField = driver.findElement(By.id("password"));
-        passwordInputField.sendKeys("SuperSecretPassword!");
+    public void verifyLogin() {
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
 
         WebElement loginButton = driver.findElement(By.cssSelector("#login > button"));
         loginButton.click();
@@ -68,15 +70,9 @@ public class LoginTest {
         Assert.assertEquals(currentUrl,"http://the-internet.herokuapp.com/secure", "Login failed");
     }
 
-    public void checkOpenFormAuthentication(){
-        WebElement loginPageHeader = driver.findElement(By.className("example"));
-        String loginPageHeaderText = loginPageHeader.findElement(By.tagName("h2")).getText();
-        Assert.assertEquals(loginPageHeaderText, "Login Page", "Login Page is not opened by link.");
-    }
-    public void setWebDriverPath() {
-
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-
+    public String getFormAuthenticationHeadText(){
+        String loginPageHeaderText = driver.findElement(By.className("example")).getText();
+        return loginPageHeaderText;
     }
 
     @AfterMethod
